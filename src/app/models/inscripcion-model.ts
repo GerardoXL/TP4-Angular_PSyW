27577c5af9871832910e8860fe010950d5c237de
Gerardo Aramayo <leon.capo2000@gmail.com>
2026-05-10 19:00:00 -0300

    se agrego el los directorios models y services con sus respectivos archivos mas la interfaz del objeto y sus metodos

---
+++ src/app/models/inscripcion-model.ts
@@ -0,0 +1,8 @@
+export interface InscripcionModel{  //datos del alumno
+    dni: number;
+    precio: number;
+    categoriaAlumno: number; // 1= Estudiante, 2= Egresado, 3=particular
+    fechaInscripcion: string;
+    email: string;
+    curso: string;
+}
