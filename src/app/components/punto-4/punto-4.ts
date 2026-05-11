6aaddd5b83ad7e7e5c96886ef753fc4b61b513b9
MiguelAbregu <miguel_af_555@hotmail.com>
2026-05-11 13:05:01 -0300

    creacion del componente punto-4 y formulario

---
+++ src/app/components/punto-4/punto-4.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-punto-4',
+  imports: [],
+  templateUrl: './punto-4.html',
+  styleUrl: './punto-4.css',
+})
+export class Punto4 {}
