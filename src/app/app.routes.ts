03f6341dc3a7909f1d5f445a9a85cc5171b48eee
Ezequiel Villalba <ezequielvillagf@gmail.com>
2026-05-10 22:45:58 -0300

    mplementar listado de inscripciones y resumen de categorías

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Punto1 } from "./components/punto-1/punto-1";
 import { Punto2 } from "./components/punto-2/punto-2";
 import { Punto3 } from "./components/punto-3/punto-3";
 import { Component } from '@angular/core';
+import { ListadoComponent } from './components/listado/listado';
 
 export const routes: Routes = [
     {path: 'punto1', component: Punto1},
     {path: 'punto2', component: Punto2},
-    {path: 'punto3', component: Punto3}
+    {path: 'punto3', component: Punto3},
+    { path: 'listado', component: ListadoComponent }
 ];
